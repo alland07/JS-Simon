8d44e1f639a363eb0b9236bdbfe1fe7e16bf4045
Allan Duche <allan.duche@gmail.com>
2019-11-21 13:15:10 +0100

    génération d'un chiffre

--- script.js
+++ script.js
@@ -7,38 +7,53 @@ const $rules = document.querySelector('.modalRules');
 //Ces 4 fonctions vont permettre de toggle afin d'avoir comme une animation
 
 function Rouge() {
-    redbtn.classList.toggle('.rouge2');
+    redbtn.classList.toggle('rouge2');
 }
 function Bleu() {
-    bluebtn.classList.toggle('.bleu2');
+    bluebtn.classList.toggle('bleu2');
 }
 function Vert(){
-    greenbtn.classList.toggle('.vert2');
+    greenbtn.classList.toggle('vert2');
 }
 function Jaune(){
-    yellowbtn.classList.toggle('.jaune2');
+    yellowbtn.classList.toggle('jaune2');
 }
+
+
+
 // Cette fonction va executer le jeu: c'est la fonction principale du jeu
-function Execute(){
-    let couleur = console.log(Math.trunc(1 + Math.random()*4));
 
-    for(i=0;i<4;i++){
-        if(couleur=1){
-            Rouge();
-        }
-        else if(couleur=2){
-            Bleu();
-        }
-        else if(couleur=3){
-            Vert();
-        }
-        else if(couleur=4){
-            Jaune();
-        }
-    }
-    
+function play(){
+    //Génération de chiffre entre 1 et 4 afin que le programme sache quelle couleur allumé
+function generateur(){
+    return Math.trunc(1 + Math.random()*4);
 }
 
+generateur();
+console.log(generateur());
+let $generator = generateur();
+
+
+if (generateur()==1){
+    Rouge();
+}
+else if (generateur()==2){
+    Bleu();
+}
+else if (generateur()==3){
+    Vert();
+}
+else if (generateur()==4){
+    Jaune();
+}
+else{
+    console.log('Error');
+}
+
+}
+
+
+//Le modal
 function rules(){
     $rules.style.display="block";
 }
