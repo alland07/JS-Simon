9b81f2d5fd7468ee4f7d4dd068a2a9a497c95b0f
Allan Duche <allan.duche@gmail.com>
2019-11-24 15:24:31 +0100

    ajout son

--- script.js
+++ script.js
@@ -5,14 +5,16 @@ const greenbtn = document.querySelector('.vert');
 const $rules = document.querySelector('.modalRules');
 let tr = 0;
 let nbr = 0;
+const $do = document.querySelector('#audiodo');
+const $re = document.querySelector('#audiore');
+const $mi = document.querySelector('#audiomi');
+const $fa = document.querySelector('#audiofa');
 
 
 
 //Compteur de clic
-
 const $resultat = document.querySelector('.resultat');
 let $compte = parseInt(Number($resultat.innerHTML));
-
 redbtn.addEventListener('click', function (){
     $compte +=1;
     $resultat.innerHTML = $compte;
@@ -21,7 +23,7 @@ bluebtn.addEventListener('click', function (){
     $compte +=1;
     $resultat.innerHTML = $compte;
 })
-greenbtn.addEventListener('click', function (){
+greenbtn.addEventListener('click', function (){ 
     $compte +=1;
     $resultat.innerHTML = $compte;
 })
@@ -31,35 +33,38 @@ yellowbtn.addEventListener('click', function (){
 })
 
 //Ces 4 fonctions vont permettre de toggle afin d'avoir comme une animation
-
 function Rouge() {
     redbtn.className='rouge2';
     setTimeout(function passer(){
         redbtn.className='rouge';
     }, 260);
+    $do.play();
 }
 function Bleu() {
     bluebtn.className='bleu2';
     setTimeout(function passer2(){
         bluebtn.className='bleu';
     }, 260);
+    $re.play();
 }
 function Vert() {
     greenbtn.className='vert2';
     setTimeout(function passer3(){
         greenbtn.className='vert';
     }, 260);
+    $mi.play();
 }
 function Jaune() {
     yellowbtn.className='jaune2';
     setTimeout(function passer4(){
         yellowbtn.className='jaune';
     }, 260);
+    $fa.play();
 }
 
 
 
-// Cette fonction va executer le jeu: c'est la fonction principale du jeu
+// -----------------------------FONCION PLAY----------------------------------
 
 function play(){
     //Génération de chiffre entre 1 et 4 afin que le programme sache quelle couleur allumé
@@ -93,7 +98,7 @@ function play(){
 }
 
 
-//fonction restart Quand on a perdu.
+//-------------------------------------------------FONCTION RESTART-----------------------------------------------
 function recommencer(){
     play();
 }
