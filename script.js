4fb335ccacd0f2f7fff34ed923a3f2e74052c133
Allan Duche <allan.duche@gmail.com>
2019-11-24 12:33:31 +0100

    ajout fonction de compteur de clic

--- script.js
+++ script.js
@@ -3,6 +3,32 @@ const bluebtn = document.querySelector('.bleu');
 const yellowbtn = document.querySelector('.jaune');
 const greenbtn = document.querySelector('.vert');
 const $rules = document.querySelector('.modalRules');
+let tr = 0;
+let nbr = 0;
+
+
+
+//Compteur de clic
+
+const $resultat = document.querySelector('.resultat');
+let $compte = parseInt(Number($resultat.innerHTML));
+
+redbtn.addEventListener('click', function (){
+    $compte +=1;
+    $resultat.innerHTML = $compte;
+})
+bluebtn.addEventListener('click', function (){
+    $compte +=1;
+    $resultat.innerHTML = $compte;
+})
+greenbtn.addEventListener('click', function (){
+    $compte +=1;
+    $resultat.innerHTML = $compte;
+})
+yellowbtn.addEventListener('click', function (){
+    $compte +=1;
+    $resultat.innerHTML = $compte;
+})
 
 //Ces 4 fonctions vont permettre de toggle afin d'avoir comme une animation
 
@@ -36,7 +62,6 @@ function Jaune() {
 // Cette fonction va executer le jeu: c'est la fonction principale du jeu
 
 function play(){
-    
     //Génération de chiffre entre 1 et 4 afin que le programme sache quelle couleur allumé
     function generateur(){
         let gen =  Math.trunc(1 + Math.random()*4);
@@ -58,6 +83,11 @@ function play(){
     else{
         console.log('Erreur');
     }
+
+    for (nbr<4;nbr++;){
+        generateur();
+    }
+
 
 
 }
